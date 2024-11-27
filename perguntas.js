a7182875b8ee09fb0bf135267393538a23e3e122
CardosoManuxy <xyv.manuuc@gmail.com>
2024-11-27 08:41:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'linguagens',
+    'Qual é a lingua mais falada no mundo?',
+    'a lingua mais falada no mundo é o inglês'
+)
+
+criaCartao(
+    'história',
+    'Em que ano aconteceu a idependecia do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Monalisa?',
+    'Leonardo Da Vinci'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
